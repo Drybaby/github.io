7b88d33279cd375cf3a1fee57e7f0c05e67d0302
Drybaby <37876313+Drybaby@users.noreply.github.com>
2018-05-01 04:23:54 +0300

    Add files via upload

---
+++ js/svg.js
@@ -0,0 +1,6 @@
+var c1 = getElementById("1");
+var c2 = getElementById("2");
+var c3 = getElementById("3");
+var c4 = getElementById("4");
+var c5 = getElementById("5");
+var c6 = getElementById("6");
