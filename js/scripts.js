25632b6828db40300fc777d5641e662d7bcab8cd
Drybaby <37876313+Drybaby@users.noreply.github.com>
2018-05-02 20:04:22 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -58,6 +58,7 @@ function animationRevClick(element){
 		c1.classList.add("animateback");
 		bhome.style.display="none";
 		window.setTimeout(function(){
+			document.body.classList.add("outyellow"); 
 			c2.style.display="block";
 			c3.style.display="block";
 			c4.style.display="block";
