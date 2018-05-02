0d9051176e20c28e333cbfbc34bdf544fe51764f
Drybaby <37876313+Drybaby@users.noreply.github.com>
2018-05-02 05:17:51 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,45 @@
-//animationClick('#circle1', 'zoomIn');
+animationClick('#circle1', 'zoomIn');
+animationClick('#circle2', 'zoomIn');
+animationClick('#circle3', 'zoomIn');
+animationClick('#circle4', 'zoomIn');
+animationClick('#circle5', 'zoomIn');
+animationClick('#circle6', 'zoomIn');
+
+var outyellow=document.getElementById("outyellow");
+var outcyan=document.getElementById("outcyan");
+var outpink=document.getElementById("outpink");
+
+outyellow.style.display="none";
+outcyan.style.display="none";
+outpink.style.display="none";
 
 function animationClick(element, animation){
-  element = $(element);
-  element.click(
+	var c1 = document.getElementById("circle1");
+	var c2 = document.getElementById("circle2");
+	var c3 = document.getElementById("circle3");
+	var c4 = document.getElementById("circle4");
+	var c5 = document.getElementById("circle5");
+	var c6 = document.getElementById("circle6");
+	
+	
+    element = $(element);
+    element.click(
     function() {
-      element.addClass(' animated ' + animation);
-      //wait for animation to finish before removing classes
+      element.addClass(' animate ');
       window.setTimeout( function(){
-          element.removeClass(' animated ' + animation);
-      }, 2000);
+          element.removeClass(' animate ');
+		  document.body.classList.add("outyellow"); 
+		  c1.style.display='none';
+		  c2.style.display='none';
+		  c3.style.display='none';
+		  c4.style.display='none';
+		  c5.style.display='none';
+		  c6.style.display='none';
+      }, 1000);
     }
   );
 };
+
 
 $('#circle').click(function(){ 
             $('#circle1').toggleClass("animated zoomIn",function(){
