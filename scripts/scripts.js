8a560ae48986a0e3536ca07268b4f3a958a1dd12
Drybaby <37876313+Drybaby@users.noreply.github.com>
2018-05-21 23:41:58 +0300

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,62 @@
+$(function() {
+$('#secondarywrapper').hide();
+$('#detailsywrapper').hide();
+});
+
+$(function() {
+    $(document).click(function() {
+        $('#splash').fadeOut("slow");
+		$("body").css("background-color","white");
+		$("#secondarywrapper").fadeIn("slow");
+    });
+}); 
+
+$(function() {
+    $("#img1").click(function() {
+		$('#img1').transition({x: '-150px'}, '200' ,'snap');
+        $('#img1').transition({scale: 6}, '500' ,'ease');
+		$('#img1').transition({opacity: 0}, '100' ,'snap');
+		$('#img2').transition({opacity: 0}, '100' ,'snap');
+		$('#img3').transition({opacity: 0}, '100' ,'snap');
+		$('#img4').transition({opacity: 0}, '100' ,'snap');
+		
+		
+    });
+}); 
+
+$(function() {
+    $("#img2").click(function() {
+		$('#img2').transition({x: '150px'}, '200' ,'snap');
+        $('#img2').transition({scale: 6}, '500' ,'ease');
+		$('#img1').transition({opacity: 0}, '100' ,'snap');
+		$('#img2').transition({opacity: 0}, '100' ,'snap');
+		$('#img3').transition({opacity: 0}, '100' ,'snap');
+		$('#img4').transition({opacity: 0}, '100' ,'snap');
+		
+		
+    });
+}); 
+$(function() {
+    $("#img3").click(function() {
+		$('#img3').transition({x: '200px'}, '200' ,'snap');
+        $('#img3').transition({scale: 6}, '500' ,'ease');
+		$('#img1').transition({opacity: 0}, '100' ,'snap');
+		$('#img2').transition({opacity: 0}, '100' ,'snap');
+		$('#img3').transition({opacity: 0}, '100' ,'snap');
+		$('#img4').transition({opacity: 0}, '100' ,'snap');
+		
+		
+    });
+}); 
+$(function() {
+    $("#img4").click(function() {
+		$('#img4').transition({x: '-200px'}, '200' ,'snap');
+        $('#img4').transition({scale: 6}, '500' ,'ease');
+		$('#img1').transition({opacity: 0}, '100' ,'snap');
+		$('#img2').transition({opacity: 0}, '100' ,'snap');
+		$('#img3').transition({opacity: 0}, '100' ,'snap');
+		$('#img4').transition({opacity: 0}, '100' ,'snap');
+		
+		
+    });
+}); 
