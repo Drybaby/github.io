72a11556ee3230a1ee9291c83c062068f1db5c21
Drybaby <37876313+Drybaby@users.noreply.github.com>
2018-05-22 17:42:31 +0300

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -57,8 +57,7 @@ $(function() {
 		$('#img2').transition({opacity: 0}, '100' ,'snap');
 		$('#img3').transition({opacity: 0}, '100' ,'snap');
 		$('#img4').transition({opacity: 0}, '100' ,'snap');
-	    	$("body").css("background-color","black");
-	    	$("body").css("opacity","0.6");
+	    	$("body").css("background-color":"black","opacity":"0.6");
 		$('#detailswrapper').fadeIn("slow");
 		
     });
